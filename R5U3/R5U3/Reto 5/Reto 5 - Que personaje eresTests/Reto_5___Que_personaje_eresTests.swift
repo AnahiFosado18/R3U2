db2080e7c774f355fe8b14e87efb84caf2601295
AnahiFosado18 <diva_ani1998@hotmail.com>
2025-05-07 11:29:17 -0500

    Add files via upload

---
+++ R5U3/R5U3/Reto 5/Reto 5 - Que personaje eresTests/Reto_5___Que_personaje_eresTests.swift
@@ -0,0 +1,27 @@
+
+
+import XCTest
+@testable import ___VARIABLE_productName;:identifier___:
+
+class ___FILEBASENAMEASIDENTIFIER___: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    func testExample() throws {
+        
+    }
+
+    func testPerformanceExample() throws {
+        
+        self.measure {
+           
+        }
+    }
+
+}
